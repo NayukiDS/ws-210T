97f5cb0749a930b835da5a98d9c6521febc568c1
Mamoru <jlchen2015@outlook.com>
2021-01-08 10:17:12 +0900

    updated

--- install.sh
+++ install.sh
@@ -15,7 +15,12 @@ echo "installing vim-plug for ${_VIM}"
 sh -c "curl -fLo $_PLUG --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
-# mkdir -p $HOME/.config/nvim
-mkdir -p $HOME/.vim/plug
+mkdir -p $HOME/.config/nvim
+# mkdir -p $HOME/.vim/plug
 
-curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/config.vim > $HOME/.vim/plug/config.vim
+curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/init.vim > $HOME/.config/nvim/init.vim
+# curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/config.vim > $HOME/.vim/plug/config.vim
+
+_CUSTOM="$HOME/.config/nvim/custom"
+
+curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/custom/basic.vim > $_CUSTOM/basic.vim

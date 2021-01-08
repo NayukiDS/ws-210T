fc46802cd0cc747a6fe4f2b1de46025f68a5c877
Mamoru <jlchen2015@outlook.com>
2021-01-08 22:11:34 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/install.sh | sh
+# pacman -Sy && pacman -S --noconfirm curl git neovim nodejs-lts-fermium icu yarn
+
+# curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/NayukiDS/ws-210T/main/install.sh | sh
 
 _PLUG="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
 _VIM=nvim
@@ -24,5 +26,7 @@ curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/init.vim > $HOME/.c
 _CUSTOM="$HOME/.config/nvim/custom"
 
 curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/custom/basic.vim > $_CUSTOM/basic.vim
+curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/custom/cocConf.vim > $_CUSTOM/cocConf.vim
 
-sh -c "$_VIM +PlugInstall +qall"
+# sh -c "$_VIM +PlugInstall +qall"
+sh -c "$_VIM -c 'PlugInstall | qall'"

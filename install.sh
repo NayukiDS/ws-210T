d1cc210557012d4b966e4b23dbc44f18eb13a5a0
Mamoru <jlchen2015@outlook.com>
2021-01-09 12:58:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+# for arch linux
 # pacman -Sy && pacman -S --noconfirm curl git neovim nodejs-lts-fermium icu yarn
 
-# curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/NayukiDS/ws-210T/main/install.sh | sh
+# curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/NayukiDS/ws-210T/main/install.sh | bash
 
 _PLUG="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
 _VIM=nvim
@@ -25,8 +26,16 @@ curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/init.vim > $HOME/.c
 
 _CUSTOM="$HOME/.config/nvim/custom"
 
-curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/custom/basic.vim > $_CUSTOM/basic.vim
-curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/custom/cocConf.vim > $_CUSTOM/cocConf.vim
+confs=(
+    "utils"
+    "basic"
+    "cocConf"
+    "gitgutterConf"
+)
+
+for conf in "${confs[@]}"; do
+    curl https://raw.githubusercontent.com/NayukiDS/ws-210T/main/custom/$conf.vim > $conf/$.vim
+done
 
 # sh -c "$_VIM +PlugInstall +qall"
 sh -c "$_VIM -c 'PlugInstall | qall'"
